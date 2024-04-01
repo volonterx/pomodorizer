b8502787d74d8697103645308062f7cf2fe4f3a2
Andrii Artemiev <c-aartemiev@springbig.com>
2024-04-02 00:56:53 +0300

    CORS support

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,12 +5,9 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins "example.com"
-#
-#     resource "*",
-#       headers: :any,
-#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-#   end
-# end
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins /localhost/, "pomodorizer-f6ab6d5f16aa.herokuapp.com"
+    resource '*', headers: :any, methods: [:get, :post, :patch, :put, :delete, :options], credentials: true
+  end
+end
