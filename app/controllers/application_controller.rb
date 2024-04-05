e939c060dbd17a83a6ea1e53399c015103cd097b
Andrii Artemiev <c-aartemiev@springbig.com>
2024-04-06 00:45:48 +0300

    Autoformat camel case parameter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,15 @@
 class ApplicationController < ActionController::API
   include ActionController::MimeResponds
 
+  before_action :underscore_params!
+
   private
 
   def current_user
     @current_user ||= User.first
+  end
+
+  def underscore_params!
+    params.deep_transform_keys!(&:underscore)
   end
 end
